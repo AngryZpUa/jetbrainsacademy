eb8a904c2d3313e82d714f8d7de3a8ecf9c24270
AngryZpUa <zpcitizen@gmail.com>
2020-07-27 17:23:56 +0300

    update

--- Hangman/Hangman/task/hangman/hangman.py
+++ Hangman/Hangman/task/hangman/hangman.py
@@ -3,7 +3,10 @@ import random
 
 
 words = ('python', 'java', 'kotlin', 'javascript')
+alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z')
 print('H A N G M A N')
+lives = 8
 word = random.choice(words)
 #output_string = 'Guess the word ' + word[0:3].ljust(len(word), '-') + ': '
 #if word == input(output_string):
@@ -11,7 +14,7 @@ word = random.choice(words)
 #else:
 #    print('You are hanged!')
 letters = set()
-for _ in range(8):
+while lives > 0:
     print()
     secret_word = word
     for i in word:
@@ -19,10 +22,22 @@ for _ in range(8):
             secret_word = str(secret_word).replace(i, '-', len(word))
     print(secret_word)
     new_char = input('Input a letter: ')
-    if new_char in word:
+    if len(new_char) != 1:
+        print('You should input a single letter')
+    elif new_char in word and new_char not in letters:
         letters.add(new_char)
-    else:
+        if set(word) == letters:
+            print()
+            print(word)
+            print('You guessed the word!')
+            print('You survived!')
+            break
+    elif new_char in word and new_char in letters:
+        print('You already typed this letter')
+    elif new_char in alphabet:
         print('No such letter in the word')
-print()
-print('Thanks for playing!')
-print("We'll see how well you did in the next stage")
+        lives -= 1
+    else:
+        print('It is not an ASCII lowercase letter')
+if lives == 0:
+    print('You are hanged!')
